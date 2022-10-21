93e5abb0375303cd3fc4c80be148acd9b1e86564
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-21 17:47:54 +0500

    d

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.lang.Character;
+
 public class Main {
+    public static String TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        char[] simbols = TEXT
+                .toLowerCase(Locale.ROOT)
+                .toCharArray();
+
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (Character currentMap : simbols)
+            if (currentMap != ' ') {
+                map.merge(currentMap, 1, (currentValue, defaultValue) -> ++currentValue);
+                System.out.println(map);
+
+            }
+
+
+
+
+
     }
+
+
+
+
 }
